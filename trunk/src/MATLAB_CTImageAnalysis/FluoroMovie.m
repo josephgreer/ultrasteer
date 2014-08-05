faa5e6bbc930c8c315c7abdc377de349ab060b34
josephdgreer@gmail.com <josephdgreer@gmail.com@33dc037f-2e13-39f8-d5be-ca5da46f038e>
2014-08-05 22:16:42 +0000

    Moved RTUltrasteer stuff to main project

--- trunk/src/MATLAB_CTImageAnalysis/FluoroMovie.m
+++ trunk/src/MATLAB_CTImageAnalysis/FluoroMovie.m
@@ -1,31 +1,27 @@
-for ii = [26 30 31 37 39 40 42 44 45 46 48 49 50 52 53]
-    clc; close all;
-    
-    display(sprintf('Sequence Number: %d', ii));
-    inDir = strcat('F:\NEEDLE_GUIDANCE_HENRIETTA_123456\__20140611_180621_062000\DR_OVERVIEW_00',num2str(ii));
-    outDir = 'F:\NEEDLE_GUIDANCE_HENRIETTA_123456\FluoroMovies';
-    idxs = strfind(inDir, '\');
-    outFile = inDir(idxs(length(idxs))+1:length(inDir));
-    inFile = dir(inDir);
-    assert(length(inFile) == 3);
-    inFile = inFile(3).name;
-    inPath = strcat(inDir,'/',inFile);
-    outPath = strcat(outDir,'/',outFile,'.avi');
-    
-    info = dicominfo(inPath);
-    data = dicomread(inPath);
-    
-    profiles = VideoWriter.getProfiles();
-    writeObj = VideoWriter(outPath, profiles(4).Name); %uncompressed AVI
-    framerate = 1000/info.FrameTime;
-    writeObj.FrameRate = framerate;
-    open(writeObj);
-    
-    for i=1:info.NumberOfFrames
-        display(['Frame ' num2str(i)]);
-        im = mat2gray(data(:,:,1,i));
+clear; clc; close all;
+
+inDir = 'C:\Users\Joey\Dropbox (Stanford CHARM Lab)\NS Porcine Cadaver Study\1 - Data\NEEDLE_GUIDANCE_HENRIETTA_123456\__20140611_180621_062000\FL_-_ANGIO_0038';
+outDir = 'C:\Users\Joey\Dropbox (Stanford CHARM Lab)\NS Porcine Cadaver Study\1 - Data\NEEDLE_GUIDANCE_HENRIETTA_123456\__20140611_180621_062000\FluoroMovies';
+idxs = strfind(inDir, '\');
+outFile = inDir(idxs(length(idxs))+1:length(inDir));;
+inFile = dir(inDir);
+assert(length(inFile) == 3);
+inFile = inFile(3).name;
+inPath = strcat(inDir,'/',inFile);
+outPath = strcat(outDir,'/',outFile,'.mp4');
+
+info = dicominfo(inPath);
+data = dicomread(inPath);
+
+writeObj = VideoWriter(outPath);
+open(writeObj);
+repeat = 10;
+
+for i=1:info.NumberOfFrames
+    im = mat2gray(data(:,:,1,i));
+    for j=1:repeat
         writeVideo(writeObj,im);
     end
-    
-    close(writeObj);
 end
+
+close(writeObj);
