59c58ce2767ce3dfa5c20953415f1278c706d8a6
josephdgreer <josephdgreer@gmail.com>
2014-08-05 22:16:42 +0000

    Moved RTUltrasteer stuff to main project

---
+++ src/RTUltrasteer/USVisualizerWidget.cpp
@@ -0,0 +1,41 @@
+#include "USVisualizerWidget.h"
+#include <vtkSmartPointer.h>
+#include <vtkSphereSource.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkActor.h>
+#include <vtkInformation.h>
+#include <vtkStreamingDemandDrivenPipeline.h>
+
+
+USVisualizerWidget::USVisualizerWidget()
+: QVTKWidget()
+{
+}
+
+QSize USVisualizerWidget::sizeHint() const
+{
+  return QSize(1050,800);
+}
+
+void USVisualizerWidget::Initialize()
+{
+  // Sphere
+  vtkSmartPointer<vtkSphereSource> sphereSource = 
+    vtkSmartPointer<vtkSphereSource>::New();
+  //sphereSource->Update();
+  vtkSmartPointer<vtkPolyDataMapper> sphereMapper =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  sphereMapper->SetInputConnection(sphereSource->GetOutputPort());
+  vtkSmartPointer<vtkActor> sphereActor = 
+    vtkSmartPointer<vtkActor>::New();
+  sphereActor->SetMapper(sphereMapper);
+
+  vtkSmartPointer<vtkRenderer> renderer = 
+    vtkSmartPointer<vtkRenderer>::New();
+  renderer->AddActor(sphereActor);
+  renderer->SetBackground(0.0, 0.0, 0.0);
+
+  this->GetRenderWindow()->AddRenderer(renderer);
+}
