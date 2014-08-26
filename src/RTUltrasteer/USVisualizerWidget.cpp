ea5ff6d73f6cd9ada72272fe1d4c6c2a0f409a65
josephdgreer <josephdgreer@gmail.com>
2014-08-26 00:12:37 +0000

    Progress and bug fixes

--- src/RTUltrasteer/USVisualizerWidget.cpp
+++ src/RTUltrasteer/USVisualizerWidget.cpp
@@ -8,7 +8,12 @@
 #include <vtkInformation.h>
 #include <vtkStreamingDemandDrivenPipeline.h>
 #include <vtkImageImport.h>
+#include <vtkVolumeRayCastMapper.h>
+#include <vtkVolumeRayCastCompositeFunction.h>
+#include <vtkVolumeRayCastMIPFunction.h>
 #include "VolumeCreator.h"
+
+using namespace Nf;
 
 
 USVisualizerWidget::USVisualizerWidget()
@@ -43,25 +48,39 @@ void USVisualizerWidget::Initialize()
 
 #else
 
-  Nf::RPFullVolumeCreator rpvc;
-  rpvc.Initialize("V:/NeedleScan/Feb13_LiverScan/Scan 1/scan", cal, Vec2d(83/1000.0, 83/1000.0), VOL_MIDDLE, 
-    Vec3d(60.0, 60.0, 60.0), Vec3d(83.0*2, 83.0*2, 83.0*2), 0.5);
+  RPFullVolumeCreator rpvc;
+  Matrix44d cal(14.8449, 0.9477, -0.0018, 0.0, 15.0061, 0.0016, 1.00, 0.0, 0.1638, 0.0166, 0.0052, 0.0, 0.0, 0.0, 0.0, 1.0);
+  rpvc.Initialize("V:/NeedleScan/Feb13_LiverScan/Scan 1/scan", cal, Vec2d(83, 83), VOL_MIDDLE, 
+    Vec3d(60.0, 60.0, 60.0), Vec3d(83.0/1000.0*2, 83.0/1000.0*2, 83.0/1000.0*2), 0.5);
+  rpvc.Start();
+
+  Vec3i dims = rpvc.GetVolumeDims();
 
   vtkSmartPointer<vtkImageImport> importer = vtkSmartPointer<vtkImageImport>::New();
-  importer->SetDataSpacing(1, 1, 1);
+  importer->SetDataSpacing(83.0*2, 83.0*2, 83.0*2);
   importer->SetDataOrigin(0, 0, 0);
-  importer->SetWholeExtent(0, width-1, 0, height-1, 0, 0);
+  importer->SetWholeExtent(0, dims.x-1, 0, dims.y-1, 0, dims.z-1);
   importer->SetDataExtentToWholeExtent();
   importer->SetDataScalarTypeToUnsignedChar();
   importer->SetNumberOfScalarComponents(1);
-  importer->SetImportVoidPointer(cImage);
+  importer->SetImportVoidPointer(rpvc.GetVolumeOriginData());
   importer->Update();
+  
+  vtkSmartPointer<vtkVolumeRayCastMIPFunction> rayCastFunction =
+    vtkSmartPointer<vtkVolumeRayCastMIPFunction>::New();
 
+  vtkSmartPointer<vtkVolumeRayCastMapper> volumeMapper =
+    vtkSmartPointer<vtkVolumeRayCastMapper>::New();
+  volumeMapper->SetInputConnection(importer->GetOutputPort(0));
+  volumeMapper->SetVolumeRayCastFunction(rayCastFunction);
 
+  vtkSmartPointer<vtkVolume> volume =
+    vtkSmartPointer<vtkVolume>::New();
+  volume->SetMapper(volumeMapper);
 
   vtkSmartPointer<vtkRenderer> renderer = 
     vtkSmartPointer<vtkRenderer>::New();
-  renderer->AddActor(sphereActor);
+  renderer->AddViewProp(volume);
   renderer->SetBackground(0.0, 0.0, 0.0);
   this->GetRenderWindow()->AddRenderer(renderer);
 
--- src/VolumeCreator/VolumeCreator.cpp
+++ src/VolumeCreator/VolumeCreator.cpp
@@ -8,6 +8,7 @@ namespace Nf {
     : m_dims(0,0,0)
     , m_spacing(0,0,0)
     , m_scale(1.0)
+    , m_im(NULL)
   {
   }
 
@@ -15,8 +16,10 @@ namespace Nf {
   {
     m_dims = Vec3i((s32)ceil(extent.x/spacing.x), (s32)ceil(extent.y/spacing.y), (s32)ceil(extent.z/spacing.z));
     m_data = (u16 *)_aligned_malloc(m_dims.x*m_dims.y*m_dims.z*sizeof(u16), 64);
+    memset(m_data, 0, m_dims.x*m_dims.y*m_dims.z*sizeof(u16));
     m_rowStride = m_dims.x;
     m_sliceStride = m_dims.x*m_dims.y;
+    m_spacing = spacing;
 
     //Matrix stores axes of volume in physical coordinates
     Matrix33d axes = Matrix33d::Diaganol(extent*0.5)*orientation;
@@ -104,14 +107,27 @@ namespace Nf {
     return Vec3d(wc.x, wc.y, wc.z);
   }
 
+  inline Vec3d rpImageCoordToWorldCoord3(const Vec2d &image, const Matrix44d &posePos, const Matrix44d &calibration, const Vec2d &start, const Vec2d &scale)
+  {
+    Vec4d world =  posePos*calibration*Vec4d(1, (image.y-start.y)*scale.y, (image.x-start.x)*scale.x, 1.0);
+    return Vec3d(world.x, world.y, world.z);
+  }
+
+  inline Vec4d rpImageCoordToWorldCoord4(const Vec2d &image, const Matrix44d &posePos, const Matrix44d &calibration, const Vec2d &start, const Vec2d &scale)
+  {
+    return posePos*calibration*Vec4d(1, (image.y-start.y)*scale.y, (image.x-start.x)*scale.x, 1.0);
+  }
+
+
   void Volume::AddFrame(const IplImage *image, const Matrix33d &pose, const Vec3d &pos, const Matrix44d &calibration, const Vec2d &mpp)
   {
     //TODO OPTIMIZE!!!!!
     const IplImage *im = image;
     if(m_scale != 1.0) {
-      IplImage *_im = cvCreateImage(cvSize((s32)(image->width*m_scale+0.5), (s32)(image->height*m_scale+0.5)), IPL_DEPTH_8U, 1);
-      cvResize(image, _im);
-      im = _im;
+      if(m_im == NULL)
+        m_im = cvCreateImage(cvSize((s32)(image->width*m_scale+0.5), (s32)(image->height*m_scale+0.5)), IPL_DEPTH_8U, 1);
+      cvResize(image, m_im);
+      im = m_im;
     }        
     Vec2d start(320,0);
     Vec2d scale(mpp.x/1000.0, mpp.y/1000.0);
@@ -120,17 +136,23 @@ namespace Nf {
 
     Matrix44d posePos = Matrix44d::FromOrientationAndTranslation(pose, pos);
 
+    //TODO:  Check that rectangle falls within bounds of volume
+
     for(s32 y=0; y<im->height; y++) {
       const u8 *psrc = (const u8 *)(image->imageData+y*image->widthStep);
       for(s32 x=0; x<im->width; x++) {
         //Map image coord to world coord
-        imc = Vec4d(1, (y-start.y)*scale.y, (x-start.x)*scale.x, 1);
-        sensor = calibration*imc;
-        world = posePos*sensor;
+        world = rpImageCoordToWorldCoord4(Vec2d(x,y), posePos, calibration, start, scale);
+
+        if(x == 320 && y == 240)
+          int x = 0;
 
         //Now map to volume coord
         grid = m_worldToVolume*world;
         gridI = Vec3i((s32)(grid.x+0.5),(s32)(grid.y+0.5),(s32)(grid.z+0.5));
+
+        DEBUG_ASSERT(0 <= gridI.x && gridI.x < m_dims.x && 0 <= gridI.y && gridI.y < m_dims.y 
+          && 0 <= gridI.z && gridI.z < m_dims.z);
 
         //Cast to u16 and write to volume
         *GetCoordData(gridI) = (u16)psrc[x];
@@ -138,11 +160,26 @@ namespace Nf {
     }
   }
 
+  Vec3i Volume::GetDims()
+  {
+    return m_dims;
+  }
+
   ////////////////////////////////////////////////////////
   //End Volume Class
   ////////////////////////////////////////////////////////
   
 #if 1
+  ////////////////////////////////////////////////////////
+  //VolumeCreator Class
+  ////////////////////////////////////////////////////////
+  VolumeCreator::~VolumeCreator()
+  {
+  }
+  ////////////////////////////////////////////////////////
+  //End VolumeCreator Class
+  ////////////////////////////////////////////////////////
+
   ////////////////////////////////////////////////////////
   //RPVolumeCreator Class
   ////////////////////////////////////////////////////////
@@ -167,6 +204,16 @@ namespace Nf {
     m_volume.Release();
   }
 
+  Vec3i RPVolumeCreator::GetVolumeDims()
+  {
+    return m_volume.GetDims();
+  }
+
+  u16 * RPVolumeCreator::GetVolumeOriginData()
+  {
+    return m_volume.GetCoordData(Vec3i(0,0,0));
+  }
+
   ////////////////////////////////////////////////////////
   //End RPVolumeCreator Class
   ////////////////////////////////////////////////////////
@@ -188,15 +235,13 @@ namespace Nf {
   {
     //TODO RPReader pointers created below are leaked.
     char temp[200];
-    sprintf(temp,"%s/scan.b8",path);
+    sprintf(temp,"%s.b8",path);
     m_rpReaders.AddReader(RPF_BPOST8, (RPReader *)new RPFileReader(temp));
-    sprintf(temp,"%s/scan.gps1",path);
+    sprintf(temp,"%s.gps1",path);
     m_rpReaders.AddGPSReader((RPGPSReaderBasic *)new RPGPSReader(temp));
 
     m_cal = calibration;
     m_mpp = mpp;
-
-
 
     RPData rp = m_rpReaders.GetNextRPData();
     if(!rp.gps.valid) {
@@ -207,22 +252,42 @@ namespace Nf {
 
     Matrix44d tPose = Matrix44d::FromCvMat(rp.gps.pose);
     Matrix33d pose = tPose.GetOrientation();
-    s32 rv = m_volume.InitializeVolume(pose, rp.gps.pos, config, extent, spacing, imscale);
-    if(rv < 0)
-      return rv;
 
+    Matrix44d posePos = Matrix44d::FromOrientationAndTranslation(pose, rp.gps.pos);
+    Vec2d start(320.0, 0.0);
+    Vec2d mppScale(mpp.x/1000.0, mpp.y/1000.0);
+
+    Vec3d x_axis = rpImageCoordToWorldCoord3(Vec2d(1.0,0.0), posePos, calibration, start, mppScale)-rpImageCoordToWorldCoord3(Vec2d(0.0,0.0), posePos, calibration, start, mppScale);  //tPose*calibration*Vec4d(1.0, 0.0, (1.0)*mpp.x, 0.0)-tPose*calibration*Vec4d(1.0, 0.0, (0.0)*mpp.x, 0.0);
+    Vec3d y_axis = rpImageCoordToWorldCoord3(Vec2d(0.0,1.0), posePos, calibration, start, mppScale)-rpImageCoordToWorldCoord3(Vec2d(0.0,0.0), posePos, calibration, start, mppScale);//tPose*calibration*Vec4d(1.0, 1.0*mpp.y, 0.0, 0.0)-tPose*calibration*Vec4d(1.0, 0.0, 0.0, 0.0);
+    x_axis = x_axis.normalized();
+    y_axis = y_axis.normalized();
+    Vec3d z_axis = y_axis.cross(x_axis);
+
+    s32 rv = m_volume.InitializeVolume(Matrix33d::FromCols(x_axis,y_axis,z_axis), rpImageCoordToWorldCoord3(Vec2d(rp.b8->width/2.0, rp.b8->height/2.0), posePos, calibration, start, mppScale), 
+      config, extent, spacing, imscale);
+
+    rp.Release();
+
+    return 0;
+  }
+
+  void RPFullVolumeCreator::Start()
+  {
+    RPData rp = m_rpReaders.GetRPData(1);
+
+    Matrix44d tPose;
+    Matrix33d pose;
     //Since this is full volume creator, iterate through all frames.
     while(rp.gps.valid) {
       tPose = Matrix44d::FromCvMat(rp.gps.pose);
       pose = tPose.GetOrientation();
 
       m_volume.AddFrame(rp.b8, pose, rp.gps.pos, m_cal, m_mpp);
-
       rp.Release();
+
+      rp = m_rpReaders.GetNextRPData();
     }
-
-
-    return 0;
+    
   }
 
   ////////////////////////////////////////////////////////
