13a721457b87b5c9dc89ba004016d85bba00a101
josephdgreer@gmail.com <josephdgreer@gmail.com@33dc037f-2e13-39f8-d5be-ca5da46f038e>
2015-02-11 00:48:39 +0000

    Made simulation code more modular

---
+++ trunk/src/MATLAB_CurveFit/NeedleSimulationsParticleFilter/initParamsForSimulation.m
@@ -0,0 +1,68 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% initParamsForSimulation
+%%% initializes parameter structure for use in needleSimulation.m
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function params = initParamsForSimulation()
+
+% setup params
+
+%simulation parameters
+params.dt = 1/10;                                                           %simulation timestep (s)
+
+% process noise parameters
+params.muOrientation = [0; 0; 0];                                           %orientation process noise mean
+params.sigmaOrientation = diag(1/2*[pi/1000, pi/1000, pi/1000]);            %orientation process noise std
+params.muPos = [0; 0; 0];                                                   %position process noise mean
+params.sigmaPos = diag(0*[1 1 1]);                                          %position process noise std
+params.muRho = 0;                                                           %needle radius of curvature process noise mean
+params.sigmaRho = 3;                                                        %needle radius of curvature process noise std
+params.muVelocity = 0;
+params.sigmaVelocity = 5;                                                   %velocity sigma
+params.simulationTime = 15;                                                 %simulate 10 seconds of insertion
+
+% ultrasound dimensions/parameters
+params.usw = 83*630*1e-3;                                                   %physical width of ultrasound frame (mm)
+params.ush = 83*480*1e-3;                                                   %phyiscal height of ultrasound frame (mm)
+params.axialMu = 0;                                                         %(>0) ahead of needle tip, (<0) behind needle tip 0 is on
+params.axialSigma = 20;                                                     %sigma of where the image plane is wrt to needle tip (mm)
+params.frameOrientationMu = [0; 0; 0];                                      %frame orientation mu
+params.frameOrientationSigma = diag([pi/1000, pi/1000, pi/1000]);           %frame orientation sigma
+
+% measurement noise parameters
+params.measurementOffsetMu = [0; 0];                                        %zero measurement noise offset
+params.measurementOffsetSigma = diag([83*20*1e-3 83*20*1e-3]);              %5 pixels of measurement noise
+params.offNeedleDopplerMu = 0.882;                                          %Doppler strength of measurement off needle distributed according to lognormal dist
+params.offNeedleDopplerSigma = 0.939;                                       %Doppler strength of measurement off needle distributed according to lognormal dist
+params.onNeedleDopplerMu = 2.33;                                            %Doppler strength of measurement on needle distributed according to lognormal dist
+params.onNeedleDopplerSigma = 0.17;                                         %Doppler strength of measurement on needle distributed according to lognormal dist
+
+% particle filter parameters
+params.doParticleFilter = 1;
+params.n = 20;                                                              %only keep track of current state
+params.np = 100;                                                            %number of particles
+params.initOrientationSigma = diag(5*[pi/1000, pi/1000, pi/100]);           %orientation sigma for initial distribution of particles
+params.initOrientationMu = [0;0;0];                                         %orientation mu for initial distribution of particles
+params.initRhoSigma = 3;                                                    %rho sigma for initial distribution of particles
+params.initRhoMu = 0;                                                       %rho mu for initial distribution of particles
+params.initPosSigma = diag([5 5 5]);                                        %pos sigma for initial distribution of particles
+params.initPosMu = [0; 0; 0];                                               %pos mu for initial distribution of particles
+params.particlesInit = 0;                                                   %are particles initialized?
+params.particleInitTime = 2;                                                %how long until the particle filter gets initialized?
+
+% drawing parameters
+params.drawMeasurement = 1; 
+params.drawUSFrame = 1;
+params.drawTipFrame = 1;
+params.drawPastTipFrames = 0;
+params.drawParticlePos = 0;
+params.drawParticleOrientation = 0;
+params.drawParticlesNs = 1;
+params.drawExpectedPos = 1;
+params.drawExpectedOrientation = 0;
+params.drawTruePos = 1;
+
+% output video parameters
+params.writeVideo = 0;
+params.videoFile = 'C:/position.avi';
+end
