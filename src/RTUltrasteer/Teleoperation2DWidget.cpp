7787cb5a0bc8fdfe839797e3cae229b7cca23525
josephdgreer <josephdgreer@gmail.com>
2015-07-02 19:18:31 +0000

    Added sound play to MouseInteractorStylePP on click. Changed default settings in RobotHardware for ports.

--- src/RTUltrasteer/Teleoperation2DWidget.cpp
+++ src/RTUltrasteer/Teleoperation2DWidget.cpp
@@ -82,7 +82,7 @@ namespace Nf
     , m_rpReaders(NULL)
   {
 
-    ADD_OPEN_FILE_PARAMETER(m_rpFile, "RP Filename", CALLBACK_POINTER(onUpdateFile, Teleoperation2DFileWidget), this, "F:/TargetScans/TargetDataSet.b8", "Any File (*.*)");
+    ADD_OPEN_FILE_PARAMETER(m_rpFile, "RP Filename", CALLBACK_POINTER(onUpdateFile, Teleoperation2DFileWidget), this, "D:/TargetScans/TargetDataSet.b8", "Any File (*.*)");
     ADD_INT_PARAMETER(m_frame, "Frame Index", CALLBACK_POINTER(onUpdateFrame, Teleoperation2DFileWidget), this, 1, 1, 100, 1);
 
     onUpdateFile();
