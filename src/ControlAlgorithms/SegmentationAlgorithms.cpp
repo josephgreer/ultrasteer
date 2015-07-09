19599a0ae8f33da378205f472b9633c0a1c3cdf4
troy.adebar <troy.adebar@gmail.com>
2015-07-09 00:19:21 +0000

    Added initial image analysis for SegmentationAlgorithms.cpp

--- src/ControlAlgorithms/SegmentationAlgorithms.cpp
+++ src/ControlAlgorithms/SegmentationAlgorithms.cpp
@@ -1,5 +1,7 @@
 #include "SegmentationAlgorithms.h"
 
+using namespace cv;
+
 namespace Nf {
 
   InPlaneSegmentation::InPlaneSegmentation()
@@ -39,9 +41,48 @@ namespace Nf {
     }
   }
 
-  void InPlaneSegmentation::processFrame(RPData)
+  void InPlaneSegmentation::processFrame(RPData data)
   {
-    int i = 1;
-  }
+     cvSaveImage("F:/bmode.jpg", data.b8);
+     cvSaveImage("F:/doppler.jpg", data.color);
+
+    CvSize s = cvSize(data.color->width,data.color->height);
+     int d = data.color->depth;
+     IplImage* R = cvCreateImage(s, d, 1);
+     IplImage* G = cvCreateImage(s, d, 1);
+     IplImage* B = cvCreateImage(s, d, 1);
+     IplImage* X = cvCreateImage(s, d, 1);
+     cvSplit(data.color, R, G, B, X);
+    
+     cvSaveImage("F:/R.jpg", R);
+     cvSaveImage("F:/G.jpg", G);
+     cvSaveImage("F:/B.jpg", B);
+     cvSaveImage("F:/X.jpg", X);
+     
+     // convert the color doppler image to a grayscale
+     IplImage* rawGray = cvCreateImage(s, d, 1);
+     cvCvtColor(data.color,rawGray,CV_RGBA2GRAY);
+     
+     // mask the color by comparing channels
+     IplImage* mask = cvCreateImage(s, d, 1);
+     IplImage* gray = cvCreateImage(s, d, 1);
+     cvZero(gray);
+     cvAbsDiff(R, G, mask);
+     cvCopy(rawGray,gray,mask);
+
+     cvSaveImage("F:/grayed.jpg",gray);
+     
+     //// iterate through the pixels, and save the coordinates of non-zero pixels
+     //for (int i = 0; i < gray->width; i++ ) {
+     //  for (int j = 0; j < gray->height; j++) {
+     //    if (img.at<uchar>(j, i) == 255) {   
+     //      cout << i << ", " << j << endl;     // Do your operations
+     //    }
+     //  }
+     //}
+   
+
+     }
+
   
 };
