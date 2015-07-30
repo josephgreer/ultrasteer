f67a3714d2f09c91c8f370cb9add1b0aa2fb3353
troy.adebar <troy.adebar@gmail.com>
2015-07-30 00:20:24 +0000

    Completed InPlaneSegmentation without any noise rejection.

--- src/ControlAlgorithms/SegmentationAlgorithms.cpp
+++ src/ControlAlgorithms/SegmentationAlgorithms.cpp
@@ -1,3 +1,8 @@
+#define I_IND   2    // z is independent
+#define I_DEP1  0    // x is dependent
+#define I_DEP2  1    // y is dependent
+#define DIR     >    // > indicates +ve is insertion, < indicates -ve is insertion
+
 #include "SegmentationAlgorithms.h"
 
 
@@ -5,6 +10,8 @@ namespace Nf {
 
   InPlaneSegmentation::InPlaneSegmentation()
   {
+    m_tipPt = arma::zeros<arma::vec>(3);
+    m_tipFrame = Matrix44d::I();
   }
 
   InPlaneSegmentation::~InPlaneSegmentation()
@@ -18,6 +25,9 @@ namespace Nf {
 
   void InPlaneSegmentation::resetManualScan()
   {
+    if( !m_dopplerPoints.empty() )
+      m_dopplerPoints.clear();
+
     RPData frame;
 
     for( int i = m_scanFrames.size(); i>0 ; i-- )
@@ -31,8 +41,10 @@ namespace Nf {
   Matrix44d InPlaneSegmentation::processManualScan()
   {
 
-    if( !m_dopplerPoints.empty() )
-      m_dopplerPoints.clear();
+    // Reset and preallocate points matrix
+    m_dopplerPoints.reset();
+    m_dopplerPoints = arma::zeros<arma::mat>(100000,3);
+    m_NdopplerPoints = 0;
 
     if( m_scanFrames.size() < 1 )
       return Matrix44d::I();
@@ -43,8 +55,10 @@ namespace Nf {
       processFrame(frame);
     }
 
-    return fitPolynomialToDoppler();
-
+    fitPolynomialandLineToDoppler();
+    findExtents();
+    measureTipFrame();
+    return Matrix44d::I();
   }
 
   // Separate combined BGR doppler + B-mode image into grayscale Doppler only and mask
@@ -133,9 +147,11 @@ namespace Nf {
     dopplerLargestBin = tempMask.clone();
   }
 
+  // Extract 3D coordinates of all Doppler points
   void InPlaneSegmentation::processFrame(RPData data)
   {
     using namespace cv;
+    using namespace arma;
 
     // create image memory
     CvSize s = cvSize(data.color->width, data.color->height);
@@ -147,9 +163,9 @@ namespace Nf {
     separateColor(data.color, gray, binary);
 
     // dilate, erode, and isolate largest blob
-    Mat gray_mat;
+    cv::Mat gray_mat;
     largestBlob(binary, gray_mat);
-    
+
     // define values to convert 2D image points to 3D using GPS data
     Matrix44d cal(14.8449, 0.9477, -0.0018, 0.0, 15.0061, 0.0016, 1.00, 0.0, 0.1638, 0.0166, 0.0052, 0.0, 0.0, 0.0, 0.0, 1.0);
     Matrix44d tPose = Matrix44d::FromCvMat(data.gps.pose);
@@ -163,7 +179,11 @@ namespace Nf {
       for (int j = 0; j < gray_mat.rows; j++) {
         if (gray_mat.at<uchar>(j, i) > 0) {   
           //NTrace("x= %d, y=%d\n", i, j);
-          m_dopplerPoints.push_back(rpImageCoordToWorldCoord3(Vec2d(i,j), posePos, cal, data.origin, scale));
+          Vec3d pt = rpImageCoordToWorldCoord3(Vec2d(i,j), posePos, cal, data.origin, scale);
+          m_dopplerPoints(m_NdopplerPoints,0) = pt.x;
+          m_dopplerPoints(m_NdopplerPoints,1) = pt.y;
+          m_dopplerPoints(m_NdopplerPoints,2) = pt.z;
+          m_NdopplerPoints++;
         }else{
           int i = 1;
         }
@@ -183,49 +203,112 @@ namespace Nf {
     cvReleaseImage(&gray);
   }
 
-  Matrix44d InPlaneSegmentation::fitPolynomialToDoppler()
+  // Fit polynomial and line to 3D Doppler coordinates
+  void InPlaneSegmentation::fitPolynomialandLineToDoppler()
   {
+    BEGIN_TIMING(CURVEFITTING,1);
+
     using namespace arma;
     if( m_dopplerPoints.size() < 1 )
-      return Matrix44d::I();
+      return;
 
-    // define the independent and dependent coordinates for curve fitting
-    int i_i=2;          //z is independent
-    int i_d1=0, i_d2=1; // x and y and dependent
+    // remove extra empty memory from the Doppler points matrix
+    m_dopplerPoints.resize(m_NdopplerPoints,3);
 
-    // format the problem as Av = b
-    mat A;
-    vec b1(m_dopplerPoints.size());
-    vec b2(m_dopplerPoints.size());
+    // to format the problem as Av = b, create the A matrices
+    mat A_lin = join_horiz(m_dopplerPoints.col(I_IND),ones<mat>(m_NdopplerPoints,1));
+    mat A_pol = ones<mat>(m_NdopplerPoints,1);
+    for( int i = 0; i<3; i++ )
+      A_pol.insert_cols(0,m_dopplerPoints.col(I_IND)%A_pol.col(0));
 
-    mat row = ones<mat>(1,4);
-    vec pt(3);
-
-    for( int i = 0; i < m_dopplerPoints.size(); i++ ){
-      // extract the current point
-      pt(0) = m_dopplerPoints[i].x;
-      pt(1) = m_dopplerPoints[i].y;
-      pt(2) = m_dopplerPoints[i].z;
-
-      // save the powers of the independent coordinate into a matrix row
-      row(0,0) = pt(i_i)*pt(i_i)*pt(i_i);
-      row(0,1) = pt(i_i)*pt(i_i);
-      row(0,2) = pt(i_i);
-      A.insert_rows(i,row);
-
-      // save the dependent coordinates into vectors
-      b1(i) = pt(i_d1);
-      b2(i) = pt(i_d2);
-    }
+    // extract the dependent coordinate columns
+    vec b1 = m_dopplerPoints.col(I_DEP1);
+    vec b2 = m_dopplerPoints.col(I_DEP2);
 
     // use the pseudo inverse of A to find the polynomial fits for dependent coordinates
-    vec v1 = pinv(A)*b1;
-    vec v2 = pinv(A)*b2;
+    m_c1poly = pinv(A_pol)*b1;
+    m_c2poly = pinv(A_pol)*b2;
+    NTrace("a_poly_x= { %f, %f, %f, %f}\n", m_c1poly(0), m_c1poly(1), m_c1poly(2), m_c1poly(3));
+    NTrace("a_poly_y= {  %f, %f, %f, %f}\n", m_c2poly(0), m_c2poly(1), m_c2poly(2), m_c2poly(3));
 
-    NTrace("a_x= { %f, %f, %f, %f}\n", v1(0), v1(1), v1(2), v1(3));
-    NTrace("a_y= {  %f, %f, %f, %f}\n", v2(0), v2(1), v2(2), v2(3));
-    return Matrix44d::I();
+    // use the pseudo inverse of A to find the linear fits for dependent coordinates
+    m_c1lin = pinv(A_lin)*b1;
+    m_c2lin = pinv(A_lin)*b2;
+    NTrace("a_lin_x= { %f, %f }\n", m_c1lin(0), m_c1lin(1));
+    NTrace("a_lin_y= {  %f, %f }\n", m_c2lin(0), m_c2lin(1));
+
+    END_TIMING(CURVEFITTING,1)
+  }
+
+  // Find the location of the tip of the best-fit polynomial
+  void InPlaneSegmentation::findExtents()
+  {
+    using namespace arma;
+
+    // Rearrange the best-fit line into p = o + s*v parameterization
+    mat o = zeros<mat>(1,3);
+    vec v = ones<vec>(3);
+    o(0,I_DEP1) = m_c1lin(1);
+    o(0,I_DEP2) = m_c2lin(1);
+    v(I_DEP1) = m_c1lin(0);
+    v(I_DEP2) = m_c2lin(0);
+    mat O = repmat(o,m_NdopplerPoints,1);
+
+    // Find the projection of each point onto the best-fit line
+    mat proj = (m_dopplerPoints-O)*v;
+
+    // Find the points that are farthest along the best-fit line
+    uword min_row, max_row, col;
+    double maxVal = proj.max(max_row, col);
+    double minVal = proj.min(min_row, col);
+
+    // Extract the minimum and maximum extent coordinates
+    mat maxPt = m_dopplerPoints.row(max_row);
+    mat minPt = m_dopplerPoints.row(min_row);
+
+    // Pick the tip point
+    if( maxPt(0,I_IND) DIR minPt(0,I_IND) )
+      m_tipPt = trans(maxPt);
+    else
+      m_tipPt = trans(minPt);
+  }
+
+  // Find the orientation of the needle tip frame based on the polynomial
+  void InPlaneSegmentation::measureTipFrame()
+  {
+    using namespace arma;
+    
+    // Find the first derivative at the tip, this is the z-axis of the tip frame
+    vec z_axis(3);
+    z_axis(I_DEP1) = m_c1poly(2) + 2*m_c1poly(1)*m_tipPt(I_IND) + 3*m_c1poly(0)*pow(m_tipPt(I_IND),2);
+    z_axis(I_DEP2) = m_c2poly(2) + 2*m_c2poly(1)*m_tipPt(I_IND) + 3*m_c2poly(0)*pow(m_tipPt(I_IND),2); 
+    z_axis(I_IND) = 1;
+    z_axis = normalise(z_axis);
+
+    // Find the second derivative at the tip, this is the y-axis and z-axis combined
+    vec y_axis(3);
+    y_axis(I_DEP1) = 2*m_c1poly(1) + 6*m_c1poly(0)*m_tipPt(I_IND);
+    y_axis(I_DEP2) = 2*m_c2poly(1) + 6*m_c2poly(0)*m_tipPt(I_IND);
+    y_axis(I_IND) = 0;
+    y_axis = normalise(y_axis);
+    
+    // Remove z-axis components from y_axis
+    y_axis = y_axis - dot(y_axis,z_axis)*z_axis;
+    y_axis = normalise(y_axis);
+
+    // Create rotation matrix representation of the frame
+    vec x_axis = cross(y_axis, z_axis);
+    mat R;
+    R.insert_cols(0,z_axis);
+    R.insert_cols(0,y_axis);
+    R.insert_cols(0,x_axis);
+
+    // Create 4x4 transformation matrix
+    mat T = R;
+    T.insert_cols(3,m_tipPt);
+    mat h_row;
+    h_row << 0 << 0 << 0 << 1;
+    T.insert_rows(3,h_row);
 
   }
-};
-
+  }
