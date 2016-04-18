85de945966cb054a9a989de6e2d9b34b5d4c3d5a
Joey <josephdgreer@gmail.com>
2016-04-18 16:58:13 -0700

    Latest changes to control

--- src/Vine/HW/Winch/Code/Control/Control.ino
+++ src/Vine/HW/Winch/Code/Control/Control.ino
@@ -18,14 +18,6 @@ int encoderPinB = 3;
 // Program Scope Variables
 Encoder encoder(encoderPinA, encoderPinB);
 f64 countPerRev = 1024;
-f64 lastPos = 0; 
-f64 lastVel = 0; 
-f64 objectNaturalPos = 0; 
-f64 objectPos = 0; 
-f64 objectLastPos = 0; 
-f64 objectVel = 0; 
-f64 objectLastVel = 0; 
-char virtualEnvCase; 
 int counter = 0; 
 
 // --------------------------
@@ -34,7 +26,7 @@ int counter = 0;
 void setup() 
 {
   // Set Up Serial
-  Serial.begin(9600);
+  Serial.begin(115200);
   
   // Set PWM frequency 
   setPwmFrequency(pwmAPin,1); //PWM on pin 3 is (32500/1) = 32500 [Hz] 
@@ -59,67 +51,216 @@ void setup()
 // --------------------------
 // Main Loop 
 // --------------------------
+enum CONTROL_MODE {
+  CM_POS = 0,
+  CM_VEL = 1,
+  CM_THROTTLE = 2
+};
+
 f64 errorLast = 0;
 u8 dirLast = 0;
 u32 lastTime = 0;
-f64 des = 0;
+f64 desPos = 0;
+f64 desVel = 0;
 u32 count = 0;
 f64 integralError = 0;
+f64 lastPos = 0;
+f64 lastVel = 0;
 
-f64 Kp = 0.08;
-f64 Kd = 0.5;
-f64 Ki = 0.00005;
+// Position Constants
+f64 Kpp = 0.08;
+f64 Kdp = 0;
+f64 Kip = 0.00005;
+
+// Velocity Constants
+f64 Kpv = 1;
+f64 Kdv = 0;
+f64 Kiv = 0.01;
+
+CONTROL_MODE controlMode = CM_POS;
+
+u8 unwindDir = 1;
+
+
 void loop() 
 {
   f64 pos = encoder.read();
+  u32 currTime = millis();
+  f64 dt = (f64)(currTime-lastTime)/(1000.0);
+  f64 u;
+  f64 vel;
+  
+  f64 error, derror;
 
-  f64 error = des-pos;
-  f64 derror = error-errorLast;
+  if(controlMode == CM_POS) {
+    error = desPos-pos;
+  } else {
+    vel = 0.2*(pos-lastPos)/dt + 0.8*lastVel;
+    error = desVel-vel;
+    lastVel = vel;
+  }
+  derror = error-errorLast;
   integralError += error;
     
   errorLast = error;
-
-  u32 currTime = millis();
-  f64 dt = (f64)(currTime-lastTime);
   derror /= dt;
   lastTime = currTime;
-    
-  f64 u = Kp*error + Kd*derror + Ki*integralError;
+  lastPos = pos;
 
-  char dir = 1;
-  if(u < 0) {
-    dir = 0;
-    u = abs(u);
+  s8 dir = 0;
+  
+  switch(controlMode) {
+    case CM_POS:
+    {
+      u = Kpp*error + Kdp*derror + Kip*integralError;
+  
+      if(u < 0) {
+        dir = 1;
+        u = abs(u);
+      }
+      u = min(u, 255);
+      break;
+    }
+    case CM_VEL:
+    {
+      u = Kpv*error + Kdv*derror + Kiv*integralError;
+  
+      if(u < 0) {
+        dir = 1;
+        u = abs(u);
+      }
+      u = min(u, 255);
+      break;
+    }
+    case CM_THROTTLE:
+    {
+      u = Kpv*error + Kdv*derror + Kiv*integralError;
+  
+      if(u < 0) {
+        dir = 1;
+        u = abs(u);
+      }
+      if(dir == unwindDir) {
+        u = 0;
+        integralError = 0;
+        derror = 0;
+        dir = 1-unwindDir;
+      }
+      u = min(u, 255);
+      break;
+    }
   }
-  u = min(u, 255);
+  
   if(dir != dirLast) {
     digitalWrite(dirAPin, dir);
     dirLast = dir;
   }
+  if(count++ % 2000 == 0 && controlMode == CM_POS)
+    Serial.println("Des Pos " +  String(desPos) + " Pos " + String(pos) + " Error " + String(error) + " u " + String(u) + " dt " + String(dt*1000.0) + " derror " + String(derror) + " integralError " + String(integralError));
+  else if(count % 2000 == 0 && controlMode == CM_VEL)
+    Serial.println("DesVel " + String(desVel) + " Vel " + String(vel) + " Dir " + String((s32)dir) + " Error " + String(error) + " u " + String(u) + " dt " + String(dt*1000.0) + " derror " + String(derror) + " integralError " + String(integralError));
+  else if(count % 2000 == 0 && controlMode == CM_THROTTLE)
+    Serial.println("DesVel " + String(desVel) + " Vel " + String(vel) + " Dir " + String((s32)dir) + " Error " + String(error) + " u " + String(u) + " dt " + String(dt*1000.0) + " derror " + String(derror) + " integralError " + String(integralError));
   analogWrite(pwmAPin, u);
-  if(count++ % 2000 == 0)
-    Serial.println("Pos " + String(pos) + " Error " + String(error) + " u " + String(u) + " dt " + String(dt) + " derror " + String(derror) + " integralError " + String(integralError));
 
-  s32 input = 0;
-  while(Serial.available()) {
-    input = Serial.read();
-    if(input == 'p') {
-      des += 2048;
-      Serial.println("Incrementing desired value to " + String(des));
-    } else if(input == 'n') {
-      des -= 2048;
-      Serial.println("Decrementing desired value to " + String(des));
-    }
+}
+
+#define BUFFER_LEN 20
+s8 input[BUFFER_LEN] = {0};
+s32 nBytes = 0;
+
+void serialEvent() 
+{
+  if(Serial.available()) {
+    nBytes = Serial.readBytesUntil('\n', input, BUFFER_LEN);
+
+    switch(input[0]) {
+      case 'm':   // switch mode 
+      {
+        String mode;
+        if(input[2] == 'v') {               //velocity
+          controlMode = CM_VEL;
+          mode = "velocity";
+        } else if(input[2] == 'p') {        //position
+          controlMode = CM_POS;
+          mode = "position";
+        } else if(input[2] == 't') {        //throttle
+          controlMode = CM_THROTTLE;
+          mode = "throttle";
+        }
+        desVel = integralError = 0;
+        errorLast = 0;
+        desPos = lastPos;
+        integralError = 0;
+        lastVel = 0;
+        
+        Serial.println("Control mode switched to " + mode);
+        break;
+      }
+      case 'q':  // decrement set point. interpretation depends on mode
+      {
+        f64 decrement = atof(input+2);
+        if(controlMode == CM_POS)
+          desPos -= decrement;
+        else
+          desVel -= decrement;
+
+        Serial.println("Decrementing " + String(decrement));
+        break;
+      }
+      case 'w':   // increment set point. interpretation depends on mode
+      {
+        f64 increment = atof(input+2);
+        if(controlMode == CM_POS)
+          desPos += increment ;
+        else
+          desVel += increment;
+          
+        Serial.println("Incrementing " + String(increment));
+        break;
+      }
+      case 'K':   //set Kp/Kd/Ki
+      case 'k':
+      {
+        f64 *Kp, *Kd, *Ki;
+        if(controlMode == CM_VEL) {
+          Kp = &Kpv;
+          Kd = &Kdv;
+          Ki = &Kiv;
+        } else {
+          Kp = &Kpp;
+          Kd = &Kdp;
+          Ki = &Kip;
+        }
+        f64 val = atof(input+3);
+        if(input[1] == 'p') {
+          *Kp = val;
+          Serial.println("Set Kp to " + String(val));
+        } else if(input[1] == 'd') {
+          *Kd = val;
+          Serial.println("Set Kd to " + String(val));
+        } else if(input[1] == 'i') {
+          *Ki = val;
+          Serial.println("Set Ki to " + String(val));
+        }
+        break;
+      }
+      case 'p':   //Pause
+      case 'P':
+      
+        desVel = integralError = 0;
+        errorLast = 0;
+        desPos = lastPos;
+        integralError = 0;
+        lastVel = 0;
+
+        Serial.println("Pause");
+        break;
+      default:
+        break;
+    }   
   }
 }
-
-// --------------------------
-// Haptic Loop 
-// --------------------------
-void hapticLoop()
-{
-}
- 
 
 // --------------------------
 // Set PWM Freq 
