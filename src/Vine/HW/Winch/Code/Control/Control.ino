16fb904238af94b044b5ab197863205e9b196389
Joey <josephdgreer@gmail.com>
2016-04-25 17:59:34 -0700

    Integrated regulators into control

--- src/Vine/HW/Winch/Code/Control/Control.ino
+++ src/Vine/HW/Winch/Code/Control/Control.ino
@@ -1,6 +1,7 @@
-// Includes 
+// Includes
 #include <TimerOne.h>
 #include <Encoder.h>
+#include <SoftwareSerial.h>
 #include <math.h>
 
 typedef double f64;
@@ -10,30 +11,39 @@ typedef unsigned long u32;
 typedef int s32;
 
 // Pin Declares
-int pwmAPin = 5;  
-int dirAPin = 8;  
-int encoderPinA = 2;
-int encoderPinB = 3; 
+int pwmAPin = 5;
+int dirAPin = 8;
+int encoderPinA = 11;
+int encoderPinB = 3;
 
 int regulator1Pin = 9;
 int regulator2Pin = 10;
 
+int slaSerialPinRx = 2;
+int slaSerialPinTx = 12;
+
 // Program Scope Variables
 Encoder encoder(encoderPinA, encoderPinB);
 f64 countPerRev = 1024;
-int counter = 0; 
+int counter = 0;
+
+SoftwareSerial slaSerial(slaSerialPinRx, slaSerialPinTx);
 
 // --------------------------
 // Initialize
 // --------------------------
-void setup() 
+void setup()
 {
+  pinMode(slaSerialPinRx, INPUT);
+
   // Set Up Serial
-  Serial.begin(9600);
-  
-  // Set PWM frequency 
-  setPwmFrequency(pwmAPin,1); //PWM on pin 3 is (32500/1) = 32500 [Hz] 
-  
+  Serial.begin(57600);
+  slaSerial.begin(57600);
+  slaSerial.setTimeout(1);
+
+  // Set PWM frequency
+  setPwmFrequency(pwmAPin, 1); //PWM on pin 3 is (32500/1) = 32500 [Hz]
+
   // Output Pins
   pinMode(pwmAPin, OUTPUT);  // PWM for A
   pinMode(dirAPin, OUTPUT);  // dir for A
@@ -45,21 +55,22 @@ void setup()
   // Encoder Pins
   pinMode(encoderPinA, INPUT);
   pinMode(encoderPinB, INPUT);
-  
-  // Init Motor 
+
+  // Init Motor
   analogWrite(pwmAPin, 0);     // set to not be spinning (0/255)
-  digitalWrite(dirAPin, LOW);  // set direction 
+  digitalWrite(dirAPin, LOW);  // set direction
 
   digitalWrite(regulator1Pin, LOW);
-  
-  // Init Timer and Set Haptic Loop 
-  //Timer1.initialize(); 
-  //long period = 10000; // 10000 [us] -> 100 Hz sample time 
-  //Timer1.attachInterrupt(hapticLoop,period); 
+  digitalWrite(regulator2Pin, HIGH);
+
+  // Init Timer and Set Haptic Loop
+  //Timer1.initialize();
+  //long period = 10000; // 10000 [us] -> 100 Hz sample time
+  //Timer1.attachInterrupt(hapticLoop,period);
 }
 
 // --------------------------
-// Main Loop 
+// Main Loop
 // --------------------------
 enum CONTROL_MODE {
   CM_POS = 0,
@@ -87,120 +98,133 @@ f64 Kpv = 1;
 f64 Kdv = 0;
 f64 Kiv = 0.01;
 
-bool printOut = false;
+bool printOutMotors = false;
+bool printOutTracks = true;
 
 CONTROL_MODE controlMode = CM_POS;
 
 u8 unwindDir = 1;
 
 
-void loop() 
+#define TRACE_COUNT_TRACKS 50
+#define TRACE_COUNT_MOTORS 50
+#define BUFFER_LEN 50
+s8 input[BUFFER_LEN] = {0};
+s32 nBytes = 0;
+
+void loop()
 {
   f64 pos = encoder.read();
   u32 currTime = millis();
-  f64 dt = (f64)(currTime-lastTime)/(1000.0);
+  f64 dt = (f64)(currTime - lastTime) / (1000.0);
   f64 u;
   f64 vel;
-  
+
   f64 error, derror;
 
-  if(controlMode == CM_POS) {
-    error = desPos-pos;
+  if (controlMode == CM_POS) {
+    error = desPos - pos;
   } else {
-    vel = 0.2*(pos-lastPos)/dt + 0.8*lastVel;
-    error = desVel-vel;
+    vel = 0.2 * (pos - lastPos) / dt + 0.8 * lastVel;
+    error = desVel - vel;
     lastVel = vel;
   }
-  derror = error-errorLast;
+  derror = error - errorLast;
   integralError += error;
-    
+
   errorLast = error;
   derror /= dt;
   lastTime = currTime;
   lastPos = pos;
 
   s8 dir = 0;
-  
-  switch(controlMode) {
+
+  switch (controlMode) {
     case CM_POS:
-    {
-      u = Kpp*error + Kdp*derror + Kip*integralError;
-  
-      if(u < 0) {
-        dir = 1;
-        u = abs(u);
+      {
+        u = Kpp * error + Kdp * derror + Kip * integralError;
+
+        if (u < 0) {
+          dir = 1;
+          u = abs(u);
+        }
+        u = min(u, 255);
+        break;
       }
-      u = min(u, 255);
-      break;
-    }
     case CM_VEL:
-    {
-      u = Kpv*error + Kdv*derror + Kiv*integralError;
-  
-      if(u < 0) {
-        dir = 1;
-        u = abs(u);
+      {
+        u = Kpv * error + Kdv * derror + Kiv * integralError;
+
+        if (u < 0) {
+          dir = 1;
+          u = abs(u);
+        }
+        u = min(u, 255);
+        break;
       }
-      u = min(u, 255);
-      break;
-    }
     case CM_THROTTLE:
-    {
-      u = Kpv*error + Kdv*derror + Kiv*integralError;
-  
-      if(u < 0) {
-        dir = 1;
-        u = abs(u);
+      {
+        u = Kpv * error + Kdv * derror + Kiv * integralError;
+
+        if (u < 0) {
+          dir = 1;
+          u = abs(u);
+        }
+        if (dir == unwindDir) {
+          u = 0;
+          integralError = 0;
+          derror = 0;
+          dir = 1 - unwindDir;
+        }
+        u = min(u, 255);
+        break;
       }
-      if(dir == unwindDir) {
-        u = 0;
-        integralError = 0;
-        derror = 0;
-        dir = 1-unwindDir;
-      }
-      u = min(u, 255);
-      break;
-    }
   }
-  
-  if(dir != dirLast) {
+
+  if (dir != dirLast) {
     digitalWrite(dirAPin, dir);
     dirLast = dir;
   }
 
-  if(printOut) 
+  if (printOutMotors)
   {
-    if(count++ % 2000 == 0 && controlMode == CM_POS)
-      Serial.println("Des Pos " +  String(desPos) + " Pos " + String(pos) + " Error " + String(error) + " u " + String(u) + " dt " + String(dt*1000.0) + " derror " + String(derror) + " integralError " + String(integralError));
-    else if(count % 2000 == 0 && controlMode == CM_VEL)
-      Serial.println("DesVel " + String(desVel) + " Vel " + String(vel) + " Dir " + String((s32)dir) + " Error " + String(error) + " u " + String(u) + " dt " + String(dt*1000.0) + " derror " + String(derror) + " integralError " + String(integralError));
-    else if(count % 2000 == 0 && controlMode == CM_THROTTLE)
-      Serial.println("DesVel " + String(desVel) + " Vel " + String(vel) + " Dir " + String((s32)dir) + " Error " + String(error) + " u " + String(u) + " dt " + String(dt*1000.0) + " derror " + String(derror) + " integralError " + String(integralError));
+    if (count++ % TRACE_COUNT_MOTORS == 0 && controlMode == CM_POS)
+      Serial.println("Des Pos " +  String(desPos) + " Pos " + String(pos) + " Error " + String(error) + " u " + String(u) + " dt " + String(dt * 1000.0) + " derror " + String(derror) + " integralError " + String(integralError));
+    else if (count % TRACE_COUNT_MOTORS == 0 && controlMode == CM_VEL)
+      Serial.println("DesVel " + String(desVel) + " Vel " + String(vel) + " Dir " + String((s32)dir) + " Error " + String(error) + " u " + String(u) + " dt " + String(dt * 1000.0) + " derror " + String(derror) + " integralError " + String(integralError));
+    else if (count % TRACE_COUNT_MOTORS == 0 && controlMode == CM_THROTTLE)
+      Serial.println("DesVel " + String(desVel) + " Vel " + String(vel) + " Dir " + String((s32)dir) + " Error " + String(error) + " u " + String(u) + " dt " + String(dt * 1000.0) + " derror " + String(derror) + " integralError " + String(integralError));
     analogWrite(pwmAPin, u);
   }
 
+#if 1
+  if (Serial.available()) {
+    nBytes = Serial.readBytes(input, BUFFER_LEN);
+    handleSerial(input, nBytes);
+    memset(input, 0, nBytes);
+  }
+#endif
+
+  if (slaSerial.available()) {
+    nBytes = slaSerial.readBytes(input, BUFFER_LEN);
+    handleSlaSerial((u8 *)input, nBytes);
+    //memset(input, 0, nBytes);
+  }
 }
 
-#define BUFFER_LEN 20
-s8 input[BUFFER_LEN] = {0};
-s32 nBytes = 0;
-
-void serialEvent() 
+void handleSerial(const s8 *input, s32 nBytes)
 {
-  if(Serial.available()) {
-    nBytes = Serial.readBytesUntil('\n', input, BUFFER_LEN);
-
-    switch(input[0]) {
-      case 'm':   // switch mode 
+  switch (input[0]) {
+    case 'm':   // switch mode
       {
         String mode;
-        if(input[2] == 'v') {               //velocity
+        if (input[2] == 'v') {              //velocity
           controlMode = CM_VEL;
           mode = "velocity";
-        } else if(input[2] == 'p') {        //position
+        } else if (input[2] == 'p') {       //position
           controlMode = CM_POS;
           mode = "position";
-        } else if(input[2] == 't') {        //throttle
+        } else if (input[2] == 't') {       //throttle
           controlMode = CM_THROTTLE;
           mode = "throttle";
         }
@@ -209,14 +233,14 @@ void serialEvent()
         desPos = lastPos;
         integralError = 0;
         lastVel = 0;
-        
+
         Serial.println("Control mode switched to " + mode);
         break;
       }
-      case 'q':  // decrement set point. interpretation depends on mode
+    case 'q':  // decrement set point. interpretation depends on mode
       {
-        f64 decrement = atof(input+2);
-        if(controlMode == CM_POS)
+        f64 decrement = atof(input + 2);
+        if (controlMode == CM_POS)
           desPos -= decrement;
         else
           desVel -= decrement;
@@ -224,22 +248,22 @@ void serialEvent()
         Serial.println("Decrementing " + String(decrement));
         break;
       }
-      case 'w':   // increment set point. interpretation depends on mode
+    case 'w':   // increment set point. interpretation depends on mode
       {
-        f64 increment = atof(input+2);
-        if(controlMode == CM_POS)
+        f64 increment = atof(input + 2);
+        if (controlMode == CM_POS)
           desPos += increment ;
         else
           desVel += increment;
-          
+
         Serial.println("Incrementing " + String(increment));
         break;
       }
-      case 'K':   //set Kp/Kd/Ki
-      case 'k':
+    case 'K':   //set Kp/Kd/Ki
+    case 'k':
       {
         f64 *Kp, *Kd, *Ki;
-        if(controlMode == CM_VEL) {
+        if (controlMode == CM_VEL) {
           Kp = &Kpv;
           Kd = &Kdv;
           Ki = &Kiv;
@@ -248,52 +272,85 @@ void serialEvent()
           Kd = &Kdp;
           Ki = &Kip;
         }
-        f64 val = atof(input+3);
-        if(input[1] == 'p') {
+        f64 val = atof(input + 3);
+        if (input[1] == 'p') {
           *Kp = val;
           Serial.println("Set Kp to " + String(val));
-        } else if(input[1] == 'd') {
+        } else if (input[1] == 'd') {
           *Kd = val;
           Serial.println("Set Kd to " + String(val));
-        } else if(input[1] == 'i') {
+        } else if (input[1] == 'i') {
           *Ki = val;
           Serial.println("Set Ki to " + String(val));
         }
         break;
       }
-      case 'p':   //Pause
-      case 'P':
-      
-        desVel = integralError = 0;
-        errorLast = 0;
-        desPos = lastPos;
-        integralError = 0;
-        lastVel = 0;
+    case 'p':   //Pause
+    case 'P':
 
-        Serial.println("Pause");
-        break;
-      case 'l':
-      case 'L': //Toggle Pin 
+      desVel = integralError = 0;
+      errorLast = 0;
+      desPos = lastPos;
+      integralError = 0;
+      lastVel = 0;
+
+      Serial.println("Pause");
+      break;
+    case 'l':
+    case 'L': //Toggle Pin
       {
-        f64 amount = atof(input+2);
-        u8 amnt = (u8)(amount*255.0);
-        Serial.println("Setting valve to " + String(amount) + " input string " + String(input) + " amount " + String(amnt)); 
-        analogWrite(regulator1Pin, amnt);
+        s32 reg = atoi(input + 2);
+        f64 amount = atof(input + 4);
+        u8 amnt = (u8)(amount * 255.0);
+        Serial.println("Setting valve " + String(reg) + " to " + String(amount) + " input string " + String(input) + " amount " + String(amnt));
+        if(reg == 1)
+          analogWrite(regulator1Pin, amnt);
+        else if(reg == 2)
+          analogWrite(regulator2Pin, amnt);
         break;
       }
-      default:
-        break;
-    }   
+    default:
+      break;
+  }
+}
+
+
+String output;
+u32 serialCount = 0;
+u32 lastTimeTracks = 0;
+void handleSlaSerial(const u8 *input, s32 nBytes)
+{
+  #if 0
+  char printStr[100] = {0};
+  s8 *strPtr = printStr;
+  for (s32 i = 0; i < nBytes; i++) {
+    strPtr += sprintf(strPtr, "%02X ", input[i]);
+  }
+  Serial.println(String(printStr) + " Length " + String(nBytes));
+  #endif
+
+  //not the correct header;
+  if (input[0] != 0x51 || input[1] != 0xAC || input[3] != 0x43)
+    return;
+
+  u16 trackRow = input[6] + (input[7] << 8);
+  u16 trackCol = input[4] + (input[5] << 8);
+
+  if((serialCount++ % TRACE_COUNT_TRACKS) == 0 && printOutTracks) {
+    u32 currTime = millis();
+    Serial.println("track row track col " + String(trackRow) + " " + String(trackCol) + " time " + String((currTime-lastTimeTracks)/(f64)TRACE_COUNT_TRACKS));
+    lastTimeTracks = currTime;
+    serialCount = 1;
   }
 }
 
 // --------------------------
-// Set PWM Freq 
+// Set PWM Freq
 // --------------------------
 void setPwmFrequency(int pin, int divisor) {
   byte mode;
-  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
-    switch(divisor) {
+  if (pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch (divisor) {
       case 1: mode = 0x01; break;
       case 8: mode = 0x02; break;
       case 64: mode = 0x03; break;
@@ -301,13 +358,13 @@ void setPwmFrequency(int pin, int divisor) {
       case 1024: mode = 0x05; break;
       default: return;
     }
-    if(pin == 5 || pin == 6) {
+    if (pin == 5 || pin == 6) {
       TCCR0B = TCCR0B & 0b11111000 | mode;
     } else {
       TCCR1B = TCCR1B & 0b11111000 | mode;
     }
-  } else if(pin == 3 || pin == 11) {
-    switch(divisor) {
+  } else if (pin == 3 || pin == 11) {
+    switch (divisor) {
       case 1: mode = 0x01; break;
       case 8: mode = 0x02; break;
       case 32: mode = 0x03; break;
