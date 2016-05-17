216c2e7016ca76c54a8b83740e53b0c6ee17a1f9
Joey <josephdgreer@gmail.com>
2016-05-16 17:48:05 -0700

    Incremental calibration is working. Added 2x2 matrices

--- src/Vine/HW/Winch/Code/Control/Common.h
+++ src/Vine/HW/Winch/Code/Control/Common.h
@@ -72,6 +72,184 @@ public:
 	{
 		return atan2(this->y, this->x);
 	}
+};  
+
+class Matrix22f64
+{
+public:
+  f64 m_data[2][2];
+
+  Matrix22f64()
+  {
+    memset(m_data, 0, sizeof(f64) * 4);
+  }
+
+  Matrix22f64(const f64 mat[2][2])
+  {
+    memcpy(m_data, mat, sizeof(f64) * 4);
+  }
+
+  Matrix22f64(f64 a11, f64 a12, f64 a21, f64 a22)
+  {
+    m_data[0][0] = a11; m_data[0][1] = a12; 
+    m_data[1][0] = a21; m_data[1][1] = a22; 
+  }
+
+  static Matrix22f64 I()
+  {
+    f64 data[2][2];
+    data[0][0] = data[1][1] = 1;
+    data[0][1] = data[1][0] = 0;
+    return Matrix22f64(data);
+  }
+
+  static Matrix22f64 Diagonal(f64 a, f64 b)
+  {
+    f64 data[2][2];
+    memset(data, 0, sizeof(f64) * 4);
+    data[0][0] = a;
+    data[1][1] = b;
+    return Matrix22f64(data);
+  }
+
+  static Matrix22f64 Diagonal(Vec2f64 values)
+  {
+    f64 data[2][2];
+    memset(data, 0, sizeof(f64) * 4);
+    data[0][0] = values.x;
+    data[1][1] = values.y;
+    return Matrix22f64(data);
+  }
+
+  f64 Trace() const
+  {
+    f64 res = this->m_data[0][0] + this->m_data[1][1];
+    return res;
+  }
+
+  static Matrix22f64 FromCols(Vec2f64 a, Vec2f64 b)
+  {
+    f64 data[2][2];
+    data[0][0] = a.x;   data[0][1] = b.x;  
+    data[1][0] = a.y;   data[1][1] = b.y;  
+    return Matrix22f64(data);
+  }
+
+  static Matrix22f64 FromRows(Vec2f64 a, Vec2f64 b)
+  {
+    f64 data[2][2];
+    data[0][0] = a.x;  data[0][1] = a.y; 
+    data[1][0] = b.x;  data[1][1] = b.y;  
+    return Matrix22f64(data);
+  }
+
+  Matrix22f64 operator=(const Matrix22f64 &rhs)
+  {
+    memcpy(&this->m_data[0][0], &rhs.m_data[0][0], sizeof(f64) * 4);
+    return *this;  // Return a reference to myself.
+  }
+
+  Matrix22f64 operator+(const Matrix22f64 &b) const
+  {
+    f64 res[2][2];
+    res[0][0] = this->m_data[0][0] + b.m_data[0][0];
+    res[0][1] = this->m_data[0][1] + b.m_data[0][1];
+    res[1][0] = this->m_data[1][0] + b.m_data[1][0];
+    res[1][1] = this->m_data[1][1] + b.m_data[1][1];
+
+    return Matrix22f64(res);
+  }
+
+  Matrix22f64 operator+=(const Matrix22f64 &b)
+  {
+    this->m_data[0][0] += b.m_data[0][0];
+    this->m_data[0][1] += b.m_data[0][1];
+    this->m_data[1][0] += b.m_data[1][0];
+    this->m_data[1][1] += b.m_data[1][1];
+
+    return *this;
+  }
+
+  Matrix22f64 operator-(const Matrix22f64 &b) const
+  {
+    f64 res[2][2];
+    res[0][0] = this->m_data[0][0] - b.m_data[0][0];
+    res[0][1] = this->m_data[0][1] - b.m_data[0][1];
+    res[1][0] = this->m_data[1][0] - b.m_data[1][0];
+    res[1][1] = this->m_data[1][1] - b.m_data[1][1];
+
+    return Matrix22f64(res);
+  }
+
+  Matrix22f64 operator*(const f64 &b) const
+  {
+    f64 res[2][2];
+    res[0][0] = this->m_data[0][0] * b;
+    res[0][1] = this->m_data[0][1] * b;
+    res[1][0] = this->m_data[1][0] * b;
+    res[1][1] = this->m_data[1][1] * b;
+    return Matrix22f64(res);
+  }
+
+  void Print() const
+  {
+    Serial.println("[" + String(m_data[0][0]) + " " + String(m_data[0][1]) + ";\n" +
+      String(m_data[1][0]) + " " + String(m_data[1][1]) + "]");
+  }
+
+  Vec2f64 Col(s32 col) const
+  {
+    return Vec2f64(m_data[0][col], m_data[1][col]);
+  }
+
+  Vec2f64 Row(s32 row) const
+  {
+    return Vec2f64(m_data[row][0], m_data[row][1]);
+  }
+
+  Matrix22f64 operator*(const Matrix22f64 &b) const
+  {
+    f64 res[2][2];
+    memset(&res[0][0], 0, sizeof(f64) * 4);
+
+    res[0][0] = this->Row(0).dot(b.Col(0));
+    res[1][0] = this->Row(1).dot(b.Col(0));
+    res[0][1] = this->Row(0).dot(b.Col(1));
+    res[1][1] = this->Row(1).dot(b.Col(1));
+
+    return Matrix22f64(res);
+  }
+
+  Vec2f64 operator*(const Vec2f64 &b) const
+  {
+    return this->Col(0)*b.x + this->Col(1)*b.y;
+  }
+
+  Matrix22f64 Transpose() const
+  {
+    f64 res[2][2];
+    res[0][0] = this->m_data[0][0];
+    res[0][1] = this->m_data[1][0];
+    res[1][0] = this->m_data[0][1];
+    res[1][1] = this->m_data[1][1];
+    
+    return Matrix22f64(res);
+  }
+
+  f64 Determinant() const
+  {
+    return m_data[0][0] * m_data[1][1] - m_data[0][1] * m_data[1][0];
+  }
+
+  Matrix22f64 Inverse() const
+  {
+    f64 res[2][2];
+    res[0][0] = m_data[1][1]; res[0][1] = -m_data[0][1];
+    res[1][0] = -m_data[1][0]; res[1][1] = m_data[0][0];
+
+    Matrix22f64 rv(res);
+    return rv * (1.0 / Determinant());
+  }
 };
 
 #define TIME_LOOP(x,y) \
@@ -86,3 +264,5 @@ public:
 #define MIN(x,y) ((x) < (y) ? (x) : (y))
 #define MAX(x,y) ((x) > (y) ? (x) : (y))
 #define ABS(x) ((x) > 0 ? (x) : -(x))
+#define EPS (1e-8)
+#define CLAMP(x,a,b) (MIN(MAX(x,a),b))
--- src/Vine/HW/Winch/Code/Control/Control.ino
+++ src/Vine/HW/Winch/Code/Control/Control.ino
@@ -37,53 +37,6 @@ int counter = 0;
 
 //ServoValve extensionValve(servoValvePin);
 Servo extensionServo;
-// --------------------------
-// Initialize
-// --------------------------
-void setup()
-{
-#ifdef SLA_SERIAL
-  pinMode(slaSerialPinRx, INPUT);
-#endif
-
-  extensionServo.attach(servoValvePin);
-  extensionServo.write(closeAngle);
-
-  Serial.setTimeout(5);
-
-  // Set Up Serial
-  Serial.begin(57600);
-#ifdef SLA_SERIAL
-  slaSerial.begin(57600);
-  slaSerial.setTimeout(1);
-#else
-  Serial1.begin(57600);
-  Serial1.setTimeout(5);
-#endif
-
-  // Output Pins
-  pinMode(pwmAPin, OUTPUT);  // PWM for A
-  pinMode(dirAPin, OUTPUT);  // dir for A
-
-  //regulator Pins
-  for (s32 ii = 0; ii < N_TURN_ACT; ii++) {
-    pinMode(regulatorPins[ii], OUTPUT);
-    digitalWrite(regulatorPins[ii], LOW);
-  }
-
-  // Encoder Pins
-  pinMode(encoderPinA, INPUT);
-  pinMode(encoderPinB, INPUT);
-
-  // Init Motor
-  analogWrite(pwmAPin, 0);     // set to not be spinning (0/255)
-  digitalWrite(dirAPin, LOW);  // set direction
-
-  // Init Timer and Set Haptic Loop
-  //Timer1.initialize();
-  //long period = 10000; // 10000 [us] -> 100 Hz sample time
-  //Timer1.attachInterrupt(hapticLoop,period);
-}
 
 // --------------------------
 // Main Loop
@@ -98,6 +51,7 @@ enum STEERING_CONTROL_MODE {
   SCM_NOTHING = 0,
   SCM_CALIBRATE,
   SCM_CALIBRATE_INCREMENTAL,
+  SCM_CALIBRATE_PAUSE,
   SCM_POS
 };
 
@@ -175,13 +129,47 @@ void sortArray(f64 *a, u8 *idx, s32 nel)
   }
 }
 
+//Actuator effort
+//Actuator effect = a+(b-a)/(1+10^(d*(c-x))
+struct Sigmoid
+{
+  f64 a, b, c, d;
+  Sigmoid(f64 a, f64 b, f64 c, f64 d)
+    : a(a)
+    , b(b)
+    , c(c)
+    , d(d)
+  {
+  }
+  Sigmoid()
+    : a(0)
+    , b(0)
+    , c(0)
+    , d(0)
+  {
+  }
+};
+
 struct ActuatorCalibration
 {
   f64 angle;
   f64 strength;
+  Sigmoid sig;
+  ActuatorCalibration()
+    : angle(0)
+    , strength(0)
+    , sig()
+  {
+  }
+  ActuatorCalibration(f64 angle, f64 strength, Sigmoid sig)
+    : angle(angle)
+    , strength(strength)
+    , sig(sig)
+  {
+  }
 };
 
-ActuatorCalibration actuatorCalibs[N_TURN_ACT] = { 0 };
+ActuatorCalibration actuatorCalibs[N_TURN_ACT];
 u8 sortedAngleIdxs[N_TURN_ACT] = { 0 };
 
 f64 tempArray[N_TURN_ACT] = { 0 };
@@ -196,12 +184,12 @@ Vec2f64 integralErrorTrack(0, 0);
 
 f64 totalCalibTime = 0.800;
 f64 totalRestTime = 20;
-f64 incrementalCalibTime = 0.2;
+f64 incrementalCalibTime = 2;
 f64 calibTime = 0;
 s8 currCalibAct = -1;
 Vec2f64 calibStartTrackPos(0, 0);
 
-#define MAX_REGULATOR_PWM 220
+#define MAX_REGULATOR_PWM 255
 
 Vec2f64 trackPos;
 Vec2f64 scenePos;
@@ -210,9 +198,11 @@ Vec2f64 desTrackPos(320, 240);
 u32 lastTimeSteeringVisited = 0;
 
 u8 calibPWM = 0;
-#define CALIB_BIN_DS 0
-#define CALIB_BIN_DOWNSAMPLE 1.0
+#define CALIB_BIN_DS 2
+#define CALIB_BIN_DOWNSAMPLE 4.0
 
+#define MAP_VAL(amnt,a,b,c,d) (CLAMP((s32) ( (d*c + log10(MAX(amnt-a,EPS)) - log10(MAX(b-amnt,EPS)))/d ), 0, 255))
+#define ACTUATE_REGULATOR(r,amnt) (analogWrite(regulatorPins[r], MAP_VAL(amnt,actuatorCalibs[r].sig.a,actuatorCalibs[r].sig.b,actuatorCalibs[r].sig.c,actuatorCalibs[r].sig.d)))
 bool hasOutput = false;
 void controlSteering()
 {
@@ -294,18 +284,19 @@ void controlSteering()
     calibTime += dt;
     if (calibTime >= incrementalCalibTime*(calibPWM >> CALIB_BIN_DS))
       incrementBin = true;
+    else
+      calibStartTrackPos += scenePos;
 
-    if ((calibTime >= incrementalCalibTime*(MAX_REGULATOR_PWM/CALIB_BIN_DOWNSAMPLE) + totalRestTime) || currCalibAct == -1) {
+    if ((calibTime >= incrementalCalibTime*(MAX_REGULATOR_PWM / CALIB_BIN_DOWNSAMPLE) + totalRestTime) || currCalibAct == -1) {
       Serial.println("Done resting actuator " + String((s32)currCalibAct));
       doneResting = true;
       incrementBin = false;
+    } else if (calibTime >= incrementalCalibTime*(MAX_REGULATOR_PWM / CALIB_BIN_DOWNSAMPLE)) {
+      return;
     }
 
-    if ((calibTime >= incrementalCalibTime*(MAX_REGULATOR_PWM/CALIB_BIN_DOWNSAMPLE)) && (calibTime <= incrementalCalibTime*(MAX_REGULATOR_PWM/CALIB_BIN_DOWNSAMPLE) + totalRestTime))
-      return;
-
     if (incrementBin) {
-      Vec2f64 delta = scenePos - calibStartTrackPos;
+      Vec2f64 delta = scenePos + calibStartTrackPos;
       Serial.println(String((s32)currCalibAct) + ", " + String(calibPWM) + ", " + String(delta.x) + ", " + String(delta.y));
 #if 0 
       calibTrackDisplacements[currCalibAct][(calibPWM >> CALIB_BIN_DS)] = delta;
@@ -314,8 +305,10 @@ void controlSteering()
       if (calibPWM >= MAX_REGULATOR_PWM)
         calibPWM = 0;
 
-      analogWrite(regulatorPins[currCalibAct], calibPWM);
-      calibStartTrackPos = scenePos;
+      ACTUATE_REGULATOR(currCalibAct, calibPWM/256.0);
+      //analogWrite(regulatorPins[currCalibAct], calibPWM);
+
+      calibStartTrackPos = Vec2f64(0,0);
     }
     if (doneResting) {
       if (currCalibAct >= 0)
@@ -339,11 +332,16 @@ void controlSteering()
         }
 #endif
       }
-      calibStartTrackPos = scenePos;
+      calibStartTrackPos = Vec2f64(0,0);
       analogWrite(regulatorPins[currCalibAct], 0);
       calibTime = 0;
     }
 
+    break;
+  }
+  case SCM_CALIBRATE_PAUSE:
+  {
+    calibStartTrackPos = trackPos;
     break;
   }
   case SCM_POS:
@@ -515,6 +513,50 @@ void controlMotors(f64 dt)
   }
 }
 
+#if 0
+const u8 crc8_Table[] =
+{
+  0, 94, 188, 226, 97, 63, 221, 131, 194, 156, 126, 32, 163, 253, 31, 65,
+  157, 195, 33, 127, 252, 162, 64, 30, 95, 1, 227, 189, 62, 96, 130, 220,
+  35, 125, 159, 193, 66, 28, 254, 160, 225, 191, 93, 3, 128, 222, 60, 98,
+  190, 224, 2, 92, 223, 129, 99, 61, 124, 34, 192, 158, 29, 67, 161, 255,
+  70, 24, 250, 164, 39, 121, 155, 197, 132, 218, 56, 102, 229, 187, 89, 7,
+  219, 133, 103, 57, 186, 228, 6, 88, 25, 71, 165, 251, 120, 38, 196, 154,
+  101, 59, 217, 135, 4, 90, 184, 230, 167, 249, 27, 69, 198, 152, 122, 36,
+  248, 166, 68, 26, 153, 199, 37, 123, 58, 100, 134, 216, 91, 5, 231, 185,
+  140, 210, 48, 110, 237, 179, 81, 15, 78, 16, 242, 172, 47, 113, 147, 205,
+  17, 79, 173, 243, 112, 46, 204, 146, 211, 141, 111, 49, 178, 236, 14, 80,
+  175, 241, 19, 77, 206, 144, 114, 44, 109, 51, 209, 143, 12, 82, 176, 238,
+  50, 108, 142, 208, 83, 13, 239, 177, 240, 174, 76, 18, 145, 207, 45, 115,
+  202, 148, 118, 40, 171, 245, 23, 73, 8, 86, 180, 234, 105, 55, 213, 139,
+  87, 9, 235, 181, 54, 104, 138, 212, 149, 203, 41, 119, 244, 170, 72, 22,
+  233, 183, 85, 11, 136, 214, 52, 106, 43, 117, 151, 201, 74, 20, 246, 168,
+  116, 42, 200, 150, 21, 75, 169, 247, 182, 232, 10, 84, 215, 137, 107, 53
+};
+
+void sendSlaTrackingPacket(u8 *buffer, s32 trackRow, s32 trackCol)
+{
+  buffer[0] = 0x51;
+  buffer[1] = 0xAC;
+  buffer[2] = 0x07;
+
+  buffer[3] = 0x05;
+  buffer[4] = (u8)(trackCol & 0xFF);
+  buffer[5] = (u8)((trackCol & 0xFF00) >> 8);
+  buffer[6] = (u8)(trackRow & 0xFF);
+  buffer[7] = (u8)((trackRow & 0xFF00) >> 8);
+  buffer[8] = (0x40) | (0x01);
+
+  u8 crc = 1;
+  for (s32 i = 3; i <= 8; i++)
+    crc = crc8_Table[crc ^ (buffer[i])];
+
+  buffer[9] = crc;
+
+  Serial1.write(buffer, 10);
+}
+#endif
+
 void handleSerial(const s8 *input, s32 nBytes)
 {
   switch (input[0]) {
@@ -551,7 +593,12 @@ void handleSerial(const s8 *input, s32 nBytes)
     }
     else if(input[2] == 'i') {
       steeringControlMode = SCM_CALIBRATE_INCREMENTAL;
+      currCalibAct = (s32)(atoi(&input[4]));
       mode = "calibration incremental";
+    }
+    else if (input[2] == 'r') {
+      steeringControlMode = SCM_CALIBRATE_PAUSE;
+      mode = "calibration paused";
     }
     else if (input[2] == 'p') {
       steeringControlMode = SCM_POS;
@@ -560,6 +607,12 @@ void handleSerial(const s8 *input, s32 nBytes)
     else if (input[2] == 'n') {
       steeringControlMode = SCM_NOTHING;
       mode = 'none';
+      calibPWM = 0;
+      calibStartTrackPos = Vec2f64(0, 0);
+      calibTime = 0;
+      currCalibAct = 0;
+      for (s32 jj = 0; jj < N_TURN_ACT; jj++)
+        analogWrite(regulatorPins[jj], 0);
     }
     else if (input[2] == 'k') {
       f64 *Kp, *Kd, *Ki;
@@ -662,16 +715,14 @@ void handleSerial(const s8 *input, s32 nBytes)
     if (reg < 1 || reg > N_TURN_ACT)
       return;
     f64 amount = atof(input + 4);
-    u8 amnt = (u8)(amount * MAX_REGULATOR_PWM);
-    Serial.println("Setting valve " + String(reg) + " to " + String(amount) + " input string " + String(input) + " amount " + String(amnt));
-    analogWrite(regulatorPins[reg - 1], amnt);
+    Serial.println("Setting valve " + String(reg) + " to " + String(amount) + " input string " + String(input));
+    ACTUATE_REGULATOR(reg - 1, amount);
     break;
   }
   default:
     break;
   }
 }
-
 
 String output;
 u32 serialCount = 0;
@@ -707,4 +758,75 @@ void handleSlaSerial(const u8 *input, s32 nBytes)
     lastTimeTracks = currTime;
     serialCount = 1;
   }
+}
+
+// --------------------------
+// Initialize
+// --------------------------
+void setup()
+{
+#ifdef SLA_SERIAL
+  pinMode(slaSerialPinRx, INPUT);
+#endif
+
+  extensionServo.attach(servoValvePin);
+  extensionServo.write(closeAngle);
+
+  Serial.setTimeout(5);
+
+  // Set Up Serial
+  Serial.begin(57600);
+#ifdef SLA_SERIAL
+  slaSerial.begin(57600);
+  slaSerial.setTimeout(1);
+#else
+  Serial1.begin(57600);
+  Serial1.setTimeout(5);
+#endif
+
+  // Output Pins
+  pinMode(pwmAPin, OUTPUT);  // PWM for A
+  pinMode(dirAPin, OUTPUT);  // dir for A
+
+                             //regulator Pins
+  for (s32 ii = 0; ii < N_TURN_ACT; ii++) {
+    pinMode(regulatorPins[ii], OUTPUT);
+    digitalWrite(regulatorPins[ii], LOW);
+  }
+
+  // Encoder Pins
+  pinMode(encoderPinA, INPUT);
+  pinMode(encoderPinB, INPUT);
+
+  // Init Motor
+  analogWrite(pwmAPin, 0);     // set to not be spinning (0/255)
+  digitalWrite(dirAPin, LOW);  // set direction
+
+#if 0
+                               // Set up actuator calibrations
+  actuatorCalibs[0] = ActuatorCalibration(0, 0, Sigmoid(0.008272, 0.987201, 124.739665, 0.036233));
+  actuatorCalibs[1] = ActuatorCalibration(0, 0, Sigmoid(0.000489, 0.987666, 188.158080, 0.027322));
+  actuatorCalibs[2] = ActuatorCalibration(0, 0, Sigmoid(0.007008, 0.990612, 151.824623, 0.044438));
+#else
+  actuatorCalibs[0] = ActuatorCalibration(0, 0, Sigmoid(-0.000100, 0.982211, 116.866446, 0.041995));
+  actuatorCalibs[1] = ActuatorCalibration(0, 0, Sigmoid(0.047229, 0.946368, 176.593023, 0.036252));
+  actuatorCalibs[2] = ActuatorCalibration(0, 0, Sigmoid(0.015325, 0.974421, 134.341202, 0.060189));
+#endif
+
+#if 0
+  for (s32 ii = 0; ii < N_TURN_ACT; ii++) {
+    String printStr = "Actuator ii " + String(ii) + ": ";
+    for (s32 jj = 0; jj < 256; jj++) {
+      printStr += String(MAP_VAL(jj / 256.0, actuatorCalibs[ii].sig.a, actuatorCalibs[ii].sig.b, actuatorCalibs[ii].sig.c, actuatorCalibs[ii].sig.d)) + ", ";
+    }
+    Serial.println(printStr);
+    delay(1000);
+  }
+#endif
+
+
+  // Init Timer and Set Haptic Loop
+  //Timer1.initialize();
+  //long period = 10000; // 10000 [us] -> 100 Hz sample time
+  //Timer1.attachInterrupt(hapticLoop,period);
 }
