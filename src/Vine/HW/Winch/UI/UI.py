754bed371d39b92b22afc16241da39fb0bd34237
Joey <josephdgreer@gmail.com>
2016-10-18 10:52:02 -0700

    Latest changes to TapeRobot

--- src/Vine/HW/Winch/UI/UI.py
+++ src/Vine/HW/Winch/UI/UI.py
@@ -3,6 +3,9 @@ import Tkinter
 import threading
 import time
 
+runThread = True
+doneThread = False
+
 try:
     serser = serial.Serial('/dev/tty.usbserial-A603XX9M', baudrate=115200, timeout=5)  # open serial port
 except serial.SerialException:
@@ -13,10 +16,13 @@ except serial.SerialException:
 print serser.name
 
 def read_from_port(ser):
-    while True:
+    while runThread:
         if(ser.isOpen()):
-            reading = ser.readline()
-            print(reading)
+            reading = serser.readline()
+            txt.delete(Tkinter.CURRENT,Tkinter.END)
+            txt.insert(Tkinter.CURRENT,reading)
+
+    doneThread = True
 
 
 thread = threading.Thread(target=read_from_port, args=(serser,))
@@ -44,26 +50,38 @@ textBoxVars['IncPressure'].set('0.1')
 textBoxVars['DecPressure'].set('0.1')
 
 textBoxes = {}
-textBoxes['Increment'] = Tkinter.Entry(top,textvariable=textBoxVars['Increment']).grid(row=3,column=1,sticky='W')
-textBoxes['Decrement'] = Tkinter.Entry(top,textvariable=textBoxVars['Decrement']).grid(row=4,column=1,sticky='W')
-textBoxes['IncPressure'] = Tkinter.Entry(top,textvariable=textBoxVars['IncPressure']).grid(row=5,column=1,sticky='W')
-textBoxes['DecPressure'] = Tkinter.Entry(top,textvariable=textBoxVars['IncPressure']).grid(row=6,column=1,sticky='W')
-
-blah = Tkinter.Entry(top)
-blah.setvar()
+textBoxes['Increment'] = Tkinter.Entry(top,width=5,textvariable=textBoxVars['Increment']).grid(row=3,column=1,sticky=Tkinter.W)
+textBoxes['Decrement'] = Tkinter.Entry(top,width=5,textvariable=textBoxVars['Decrement']).grid(row=4,column=1,sticky=Tkinter.W)
+textBoxes['IncPressure'] = Tkinter.Entry(top,width=5,textvariable=textBoxVars['IncPressure']).grid(row=5,column=1,sticky=Tkinter.W)
+textBoxes['DecPressure'] = Tkinter.Entry(top,width=5,textvariable=textBoxVars['IncPressure']).grid(row=6,column=1,sticky=Tkinter.W)
 
 buttons = {}
 buttons['PosMode'] = Tkinter.Button(top, text='Position Mode',command=lambda: executeCommand('m p\n')).grid(row=0,sticky='W')
-buttons['ThrottleMode'] = Tkinter.Button(top, text='Position Mode',command=lambda: executeCommand('m t\n')).grid(row=1,sticky='W')
+buttons['ThrottleMode'] = Tkinter.Button(top, text='Throttle Mode',command=lambda: executeCommand('m t\n')).grid(row=1,sticky='W')
 buttons['VelMode'] = Tkinter.Button(top, text='Velocity Mode',command=lambda: executeCommand('m v\n')).grid(row=2,sticky='W')
 buttons['Increment'] = Tkinter.Button(top, text='Increment',command=lambda: executeCommandWithArgument('w ',textBoxVars['Increment'])).grid(row=3,sticky='W')
 buttons['Decrement'] = Tkinter.Button(top, text='Decrement',command=lambda: executeCommandWithArgument('q ',textBoxVars['Decrement'])).grid(row=4,sticky='W')
 buttons['IncPressure'] = Tkinter.Button(top, text='Increment Pressure',command=lambda: executeCommandWithArgument('l ',textBoxVars['IncPressure'])).grid(row=5,sticky='W')
 buttons['DecPressure'] = Tkinter.Button(top, text='Decrement Pressure',command=lambda: executeCommandWithArgument('l ',textBoxVars['DecPressure'])).grid(row=6,sticky='W')
 
+# create text widget
+txt = Tkinter.Text(top, borderwidth=3, relief="sunken", width=60,height=5)
+txt.config(font=("consolas", 12), undo=True, wrap='word')
+txt.grid(row=7, column=0, columnspan=2, sticky="nsew", padx=2, pady=2)
+
+# create a Scrollbar and associate it with txt
+scrollb = Tkinter.Scrollbar(top, command=txt.yview)
+scrollb.grid(row=7, column=3, sticky='nsew')
+txt['yscrollcommand'] = scrollb.set
+# txt['state'] = Tkinter.DISABLED
+
 # for k in buttons:
 #     buttons[k].pack()
 
 top.mainloop()
+
+runThread = False
+while not doneThread:
+    time.sleep(0.1)
 
 serser.close()             # close port
