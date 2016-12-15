37629bee072f511a89dcaf65aa7fe8f964b615bf
Joey <josephdgreer@gmail.com>
2016-12-15 14:33:47 -0800

    RobotConrolWidget->RobotHWWidget in PorcineCadaverWidget

--- src/RTUltrasteer/PorcineCadaverWidget.cpp
+++ src/RTUltrasteer/PorcineCadaverWidget.cpp
@@ -8,83 +8,17 @@ namespace Nf
     , ResizableQWidget(parent, QSize(VIS_WIDTH,VIS_HEIGHT))
     , m_saveDataWidget(new SaveDataWidget(parent))
     , m_forceSensor(new cForceSensor())
+    , m_hwWidget(new RobotHardwareWidget(this))
   {
     m_layout = new QGridLayout(parent);
     this->setLayout(m_layout);
 
-    m_rcwidget = new RobotControlsWidget(m_robot);
-    m_layout->addWidget(m_rcwidget, 0, 0, 1, 2);
-    m_layout->addWidget(m_saveDataWidget.get(), 0, 2, 1, 1);
-
-    ADD_ACTION_PARAMETER(m_initRoll, "Initialize Roll Motor", CALLBACK_POINTER(onInitRoll, PorcineCadaverWidget), this, true); 
-    ADD_ACTION_PARAMETER(m_initInsertion, "Initialize Insertion Motor", CALLBACK_POINTER(onInitInsertion, PorcineCadaverWidget), this, true); 
-    ADD_ACTION_PARAMETER(m_initArticulation, "Initialize Articulation Motor", CALLBACK_POINTER(onInitArticulation, PorcineCadaverWidget), this, true); 
-
-    ADD_CHILD_COLLECTION(m_saveDataWidget.get())
- 
-    ADD_INT_PARAMETER(m_rollCom, "Roll Port", NULL, this, 7, 1, 9, 1);   
-    ADD_INT_PARAMETER(m_insertionCom, "Insertion Port", NULL, this, 6, 1, 9, 1);
-    ADD_INT_PARAMETER(m_articulationCom, "Articulation Port", NULL, this, 8, 1, 9, 1);
-    ADD_FLOAT_PARAMETER(m_dcInsertDist, "DC Insert Dist", CALLBACK_POINTER(onSetDCInsertDist, PorcineCadaverWidget), this, 100, 20, 150, 1.0);
+    m_layout->addWidget(m_hwWidget.get(), 0, 0, 1, 1);
+    m_layout->addWidget(m_saveDataWidget.get(), 0, 1, 1, 1);
   }
 
   PorcineCadaverWidget::~PorcineCadaverWidget()
   {
-  }
-
-  void PorcineCadaverWidget::onInitRoll()
-  {
-
-    QString port;
-    QTextStream(&port) << "COM" << m_rollCom->GetValue();
-    m_robot->InitializeRoll(port);
-    m_rcwidget->enableRoll(true);
-
-  }
-  
-  void PorcineCadaverWidget::onInitInsertion()
-  {
-
-    QString port;
-    QMessageBox msgBox;
-    msgBox.setWindowTitle("HAZARD");
-    msgBox.setText("Ensure linear slide is clear before continuing to calibration.");
-    QPushButton *continueButton = msgBox.addButton("Calibrate", QMessageBox::ActionRole);
-    QPushButton *abortButton = msgBox.addButton(QMessageBox::Abort);
-    msgBox.exec();
-    if (msgBox.clickedButton() == continueButton) 
-    {
-      QTextStream(&port) << "COM" << m_insertionCom->GetValue();
-      m_robot->InitializeInsertion(port);
-      m_rcwidget->enableInsertion(true);
-    }
-
-  }
-
-  void PorcineCadaverWidget::onInitArticulation()
-  {
-
-    QString port;
-    QTextStream(&port) << "COM" << m_articulationCom->GetValue();
-    m_robot->InitializeArticulation(port);
-    m_rcwidget->enableArticulation(true);
-
-  }
-
-  void PorcineCadaverWidget::onSetDCInsertDist()
-  {
-    m_rcwidget->SetDcInsertDist(this->m_dcInsertDist->GetValue());
-  }
-
-  void PorcineCadaverWidget::setRobot(NeedleSteeringRobot* robot)
-  {
-    m_robot = robot;
-    m_rcwidget->setRobot(robot);
-  }
-
-  void PorcineCadaverWidget::UpdateGeometry()
-  {
-    this->updateGeometry();
   }
 
   void PorcineCadaverWidget::UpdateSize(QSize sz)
@@ -92,11 +26,23 @@ namespace Nf
     s32 ml, mr, mu, mb;
     ResizableQWidget::UpdateSize(sz);
     m_layout->getContentsMargins(&ml, &mu, &mr, &mb);
+    m_hwWidget->UpdateSize(QSize(3*sz.width()/4-10, sz.height()));
+    m_saveDataWidget->UpdateSize(QSize(sz.width()/4-10, sz.height()));
   }
 
   std::vector < QVTKWidget * > PorcineCadaverWidget::GetChildWidgets()
   {
     std::vector < QVTKWidget * > res;
     return res;
+  }
+
+  void PorcineCadaverWidget::UpdateGeometry()
+  {
+    this->updateGeometry();
+  }
+
+  void PorcineCadaverWidget::setRobot(NeedleSteeringRobot* robot)
+  {
+    m_hwWidget->setRobot(robot);
   }
 }
--- src/RTUltrasteer/PorcineCadaverWidget.h
+++ src/RTUltrasteer/PorcineCadaverWidget.h
@@ -5,7 +5,7 @@
 #include "UICore.h"
 #include "Resizable.h"
 #include "NeedleSteeringRobot.h"
-#include "RobotControlsWidget.h"
+#include "RobotHardwareWidget.h"
 #include "SaveDataWidget.h"
 #include "cForceSensor.h"
 
@@ -18,8 +18,8 @@ namespace Nf
   protected:
     std::tr1::shared_ptr < BoolParameter > m_init;
     QGridLayout *m_layout;
-    RobotControlsWidget *m_rcwidget;
     NeedleSteeringRobot *m_robot;
+    std::tr1::shared_ptr < RobotHardwareWidget > m_hwWidget;
     std::tr1::shared_ptr < SaveDataWidget > m_saveDataWidget;
     std::tr1::shared_ptr < cForceSensor > m_forceSensor;
 
@@ -31,44 +31,7 @@ namespace Nf
 
     std::vector < QVTKWidget * > GetChildWidgets();
 
-    //Parameters
-    //rollCom
-    std::tr1::shared_ptr < Nf::IntParameter > m_rollCom;
-    void onSetRollCom();
-    CLASS_CALLBACK(onSetRollCom, PorcineCadaverWidget);
-
-    //insertionCom
-    std::tr1::shared_ptr < Nf::IntParameter > m_insertionCom;
-    void onSetInsertionCom();
-    CLASS_CALLBACK(onSetInsertionCom, PorcineCadaverWidget);
-
-    //articulationCom
-    std::tr1::shared_ptr < Nf::IntParameter > m_articulationCom;
-    void onSetArticulationCom();
-    CLASS_CALLBACK(onSetArticulationCom, PorcineCadaverWidget);
-
-    //initRoll
-    std::tr1::shared_ptr < Nf::BoolParameter > m_initRoll;
-    void onInitRoll();
-    CLASS_CALLBACK(onInitRoll, PorcineCadaverWidget)
-
-    //initInsertion
-    std::tr1::shared_ptr < Nf::BoolParameter > m_initInsertion;
-    void onInitInsertion();
-    CLASS_CALLBACK(onInitInsertion, PorcineCadaverWidget)
-
-    //initArticulation
-    std::tr1::shared_ptr < Nf::BoolParameter > m_initArticulation;
-    void onInitArticulation();
-    CLASS_CALLBACK(onInitArticulation, PorcineCadaverWidget)
-
-    std::tr1::shared_ptr < Nf::FloatParameter > m_dcInsertDist;
-    void onSetDCInsertDist();
-    CLASS_CALLBACK(onSetDCInsertDist, PorcineCadaverWidget)
-
     void setRobot(NeedleSteeringRobot* robot);
-
-    RobotControlsWidget *GetRCWidget() { return m_rcwidget; }
   };
 }
 
